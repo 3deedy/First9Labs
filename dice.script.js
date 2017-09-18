1edbb211c8e658b2aae34a9c311a0d57475be4d4
ddmerci <31666738+ddmerci@users.noreply.github.com>
2017-09-17 21:49:52 -0400

    LAB 7: Dicey Business

--- dice.script.js
+++ dice.script.js
@@ -1,1 +1,44 @@
-“Lab 7”
+var diceArray = [];
+document.getElementById('generate-button').addEventListener('click', function() {
+    var d = new Die();
+    diceArray.push(d);
+    // diceArray.push(new Die());
+});
+
+var rollBtn = document.getElementById('roll-button');
+rollBtn.addEventListener('click', function() {
+    for (var i = 0; i < diceArray.length; i++) {
+        diceArray[i].roll();
+    }
+});
+
+document.getElementById('sum-button').addEventListener('click', function() {
+    var sum = 0;
+    for (var i = 0; i < diceArray.length; i++) {
+        sum = sum + diceArray[i].value;
+        // sum += diceArray[i].value;
+    }
+    alert('The sum of the dice is ' + sum);
+});
+
+function Die() {
+    this.div = document.createElement('div');
+    this.div.className = 'die';
+    this.div.addEventListener('click', this.roll.bind(this));
+    // this.div.addEventListener('click', function() {
+    //     this.roll();
+    // }.bind(this));
+    this.div.addEventListener('dblclick', this.removeDie.bind(this));
+    this.roll();
+    document.body.appendChild(this.div);
+}
+Die.prototype.roll = function() {
+    this.value = Math.ceil(Math.random() * 6);
+    this.div.innerHTML = this.value;
+}
+Die.prototype.removeDie = function() {
+    this.div.remove();
+
+    var index = diceArray.indexOf(this);
+    diceArray.splice(index, 1);
+}
