1bb4442bcc8837b360cc11f1aea1eac34f6bba65
“ddmerci” <31666738+ddmerci@users.noreply.github.com>
2017-09-19 21:44:17 -0400

    LAB 8: Partial Solutions Added to JS and CSS

--- canvas.script.js
+++ canvas.script.js
@@ -16,10 +16,20 @@ var shapePerimeterLabel = document.getElementById('shape-perimeter');
 var canvas = document.getElementById('canvas');
 
 document.getElementById('circle-btn').addEventListener('click', createCircle);
+document.getElementById('triangle-btn').addEventListener('click', createTriangle);
+document.getElementById('rectangle-btn').addEventListener('click', createRectangle);
 
 function createCircle() {
     var inputRadius = inputCircleRadius.value;
     new Circle(inputRadius);
+}
+
+function createTriangle() {
+  new Triangle (inputTriangleHeight.value)
+}
+
+function createRectangle() {
+  new Triangle (inputRectangleHeight.value)
 }
 
 function Shape(width, height) {
@@ -69,4 +79,61 @@ Circle.prototype.area = function() {
 
 Circle.prototype.perimeter = function() {
     return 2 * Math.PI * this.radius;
+}
+
+function Triangle (height) {
+    Shape.call(this, height, height);
+    this.cssClass = 'triangle';
+    this.draw();
+    this.div.style.width = 0;
+    this.div.style.height = 0;
+    this.div.style.borderRightWidth = height + 'px';
+    this.div.style.borderBottomHeight = height + 'px';
+
+}
+Triangle.prototype = Object.create(Shape.prototype);
+Triangle.prototype.constructor = Triangle;
+
+Triangle.prototype.area = function() {
+    return 0.5 * this.height*this.height;
+}
+
+Triangle.prototype.perimeter = function() {
+    return 2 * this.height + Math.sqrt(2 * math.pow(this.height);
+}
+
+function Rectangle (width, height) {
+    Shape.call(this, width, height);
+    this.cssClass = 'rectangle';
+    this.draw();
+    this.div.style.width = 0;
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+Rectangle.prototype.area = function() {
+    return 0.5 * this.height*this.height;
+}
+
+Rectangle.prototype.perimeter = function() {
+    return 2 * this.height + Math.sqrt(2 * math.pow(this.height);
+}
+
+function square(side) {
+  Rectangle.call(this, side, side);
+  this.cssClass = 'square';
+  this.div.classList.remove('Rectangle')
+  this.div.classList.add(this.cssClass);
+}
+
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Rectangle;
+
+Rectangle.prototype.area = function() {
+    return 0.5 * this.height*this.height;
+}
+
+Rectangle.prototype.perimeter = function() {
+    return 2 * this.height + Math.sqrt(2 * math.pow(this.height);
 }
